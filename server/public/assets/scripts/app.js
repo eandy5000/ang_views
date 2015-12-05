c78ca2bc2e167741bd98dc602944393c601fc30b
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-12-05 12:16:12 -0600

    visuals

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -7,12 +7,12 @@ myApp.config(['$routeProvider', function($routeProvider){
             templateUrl: "/assets/views/routes/home.html",
             controller: "SomeController"
         }).
-        when('/code', {
-            templateUrl: "/assets/views/routes/code.html",
+        when('/game', {
+            templateUrl: "/assets/views/routes/game.html",
             controller: "AnotherController"
         }).
-        when('/projects', {
-            templateUrl: "/assets/views/routes/projects.html",
+        when('/high', {
+            templateUrl: "/assets/views/routes/high.html",
             controller: "ThirdController"
         }).
         otherwise({
@@ -34,4 +34,68 @@ myApp.controller('AnotherController', ['$scope', function($scope){
 
 myApp.controller('ThirdController', ['$scope', function($scope){
     $scope.hello = "third controller"
+
+    $scope.players = [
+
+        {
+            Name: "Andy",
+            games: 34,
+            wins: 7,
+            streak: 2
+        },
+        {
+            Name: "Lisa",
+            games: 134,
+            wins: 40,
+            streak: 0
+        },   {
+            Name: "Tommy",
+            games: 7,
+            wins: 7,
+            streak: 7
+        },   {
+            Name: "Alice",
+            games: 134,
+            wins: 17,
+            streak: 2
+        },   {
+            Name: "Miss",
+            games: 222,
+            wins:100,
+            streak: 12
+        },   {
+            Name: "Eric",
+            games: 19,
+            wins: 7,
+            streak: 4
+        },   {
+            Name: "Jane",
+            games:80,
+            wins: 30,
+            streak: 1
+        },   {
+            Name: "Becca",
+            games: 34,
+            wins: 7,
+            streak: 2
+        },   {
+            Name: "Mary",
+            games:240,
+            wins: 99,
+            streak: 6
+        },   {
+            Name: "Frank",
+            games: 134,
+            wins: 5,
+            streak: 2
+        },   {
+            Name: "John",
+            games: 50,
+            wins: 25,
+            streak: 21
+        }
+
+
+    ];
+
 }]);
